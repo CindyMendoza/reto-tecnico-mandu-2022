46daf184859f389d60975d636f794b696dd7233b
Cindy Mendoza <mendozaic17@gmail.com>
2022-12-24 15:14:49 -0500

    first commit

---
+++ database/migrations/2022_12_24_174202_create_divisions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDivisionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('divisions', function (Blueprint $table) {
+            $table->id('division_id')->autoIncrement()->unique();
+            $table->timestamps();
+            $table->string('supdivision')->nullable();
+            $table->integer('subdivision');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('divisions');
+    }
+}
